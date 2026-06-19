7b82963a18d6c394418b145bf65b36af45036e49
Leo <me@localhost>
2026-06-19 16:49:02 +1000

    main.py api call root and /items/item_id

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from teaching-example!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id, "valid": True}
